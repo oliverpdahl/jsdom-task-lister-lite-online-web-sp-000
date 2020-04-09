1190832486cc0110cfc9e06fcf2e58f36e5d3a46
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-09 23:33:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", function(event) {
+    console.log(event.textContent)
+    // const description = document.getElementById("new-task-description");
+    // const text = description.innerText;
+    // description.innerText = ''
+    // const li = document.createElement('li')
+    // li.innerText = text
+    // document.getElementById('tasks').appendChild(li)
+    event.preventDefault();
+  });
 });
