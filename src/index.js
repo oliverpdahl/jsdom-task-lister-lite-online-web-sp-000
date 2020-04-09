c3bbbff86c857b55095d9d56698f7d35b2217c6e
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-09 23:38:44 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   document.querySelector("form").addEventListener("submit", function(event) {
     console.log(event)
-    // const description = document.getElementById("new-task-description");
-    // const text = description.value;
+    const description = document.getElementById("new-task-description");
+    const text = description.value;
     // description.innerText = ''
     // const li = document.createElement('li')
     // li.innerText = text
