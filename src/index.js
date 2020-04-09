02092dc4c7b6ed5aafb09f5eb90aaab60f49d42c
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-09 23:40:22 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector("form").addEventListener("submit", function(event) {
+    const description = document.getElementById("new-task-description");
+    const text = description.value;
+    console.log(text)
+    description.value = ''
+    const li = document.createElement('li')
+    li.innerText = text
+    document.getElementById('tasks').appendChild(li)
+    event.preventDefault();
+  });
 });
